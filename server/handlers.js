e8fa5c28ff74cc458c960fd244e5e66ee8ad677f
iescalante <ivanenriqueescalantecornejo@gmail.com>
2020-10-03 14:06:54 -0400

    ex2 done, getSeats functional with 2 methods

--- server/handlers.js
+++ server/handlers.js
@@ -1,5 +1,59 @@
-'use strict';
+"use strict";
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+const { MONGO_URI } = process.env;
 
-const getSeats = async (req, res) => {};
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+const numOfRows = 8;
+const seatsPerRow = 12;
+
+const getSeats = async (req, res) => {
+  const client = await MongoClient(MONGO_URI, options);
+  try {
+    await client.connect();
+
+    const db = client.db("ticket_booker");
+    console.log("connected!");
+
+    const data = await db.collection("seats").find().toArray();
+    console.log(data);
+    // If I use forEach() method
+    // const seatData = {};
+    // data.forEach((seat) => {
+    //   seatData[seat._id] = {
+    //     ...seat,
+    //   };
+    // });
+
+    //when using reduce method
+    const seatData = data.reduce((acc, seat) => {
+      return {
+        ...acc,
+        [seat._id]: {
+          ...seat,
+        },
+      };
+    }, {});
+
+    res.status(200).json({
+      status: 200,
+      seats: seatData,
+      numOfRows,
+      seatsPerRow,
+    });
+  } catch (err) {
+    console.log(err.stack);
+    res.status(500).json({ status: 500, data, message: err.message });
+  }
+
+  client.close();
+  console.log("disconnected!");
+};
+
+// getSeats();
 
 module.exports = { getSeats };
--- server/routes.js
+++ server/routes.js
@@ -1,59 +1,46 @@
 const router = require("express").Router();
-
-const NUM_OF_ROWS = 8;
-const SEATS_PER_ROW = 12;
+const { getSeats } = require("./handlers");
+// const NUM_OF_ROWS = 8;
+// const SEATS_PER_ROW = 12;
 
 // Code that is generating the seats.
 // ----------------------------------
-const seats = {};
-const row = ["A", "B", "C", "D", "E", "F", "G", "H"];
-for (let r = 0; r < row.length; r++) {
-  for (let s = 1; s < 13; s++) {
-    seats[`${row[r]}-${s}`] = {
-      price: 225,
-      isBooked: false,
-    };
-  }
-}
+// const seats = {};
+// const row = ["A", "B", "C", "D", "E", "F", "G", "H"];
+// for (let r = 0; r < row.length; r++) {
+//   for (let s = 1; s < 13; s++) {
+//     seats[`${row[r]}-${s}`] = {
+//       price: 225,
+//       isBooked: false,
+//     };
+//   }
+// }
 // ----------------------------------
 //////// HELPERS
-const getRowName = (rowIndex) => {
-  return String.fromCharCode(65 + rowIndex);
-};
+// const getRowName = (rowIndex) => {
+//   return String.fromCharCode(65 + rowIndex);
+// };
 
-const randomlyBookSeats = (num) => {
-  const bookedSeats = {};
+// const randomlyBookSeats = (num) => {
+//   const bookedSeats = {};
 
-  while (num > 0) {
-    const row = Math.floor(Math.random() * NUM_OF_ROWS);
-    const seat = Math.floor(Math.random() * SEATS_PER_ROW);
+//   while (num > 0) {
+//     const row = Math.floor(Math.random() * NUM_OF_ROWS);
+//     const seat = Math.floor(Math.random() * SEATS_PER_ROW);
 
-    const seatId = `${getRowName(row)}-${seat + 1}`;
+//     const seatId = `${getRowName(row)}-${seat + 1}`;
 
-    bookedSeats[seatId] = true;
+//     bookedSeats[seatId] = true;
 
-    num--;
-  }
+//     num--;
+//   }
 
-  return bookedSeats;
-};
+//   return bookedSeats;
+// };
 
 let state;
 
-router.get("/api/seat-availability", async (req, res) => {
-  if (!state) {
-    state = {
-      bookedSeats: randomlyBookSeats(30),
-    };
-  }
-
-  return res.json({
-    seats: seats,
-    bookedSeats: state.bookedSeats,
-    numOfRows: 8,
-    seatsPerRow: 12,
-  });
-});
+router.get("/api/seat-availability", getSeats);
 
 let lastBookingAttemptSucceeded = false;
 
