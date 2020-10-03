cc6bb63e382d1967b19b92f8d943853aa886a526
iescalante <ivanenriqueescalantecornejo@gmail.com>
2020-10-03 16:02:43 -0400

    ex3 completed, booking is possible, missing check for alreadyBooked

--- server/handlers.js
+++ server/handlers.js
@@ -54,6 +54,36 @@ const getSeats = async (req, res) => {
   console.log("disconnected!");
 };
 
+const bookSeat = async (req, res) => {
+  const { seatId, creditCard, expiration } = req.body;
+  const filter = { _id: seatId };
+  const updatedBooking = { $set: { isBooked: true } };
+
+  if (!creditCard || !expiration) {
+    return res.status(400).json({
+      status: 400,
+      message: "Please provide credit card information!",
+    });
+  }
+
+  const client = await MongoClient(MONGO_URI, options);
+  try {
+    await client.connect();
+
+    const db = client.db("ticket_booker");
+    console.log("connected!");
+
+    const data = await db.collection("seats").updateOne(filter, updatedBooking);
+
+    res.status(200).json({ status: 200, seatId });
+  } catch (err) {
+    console.log(err.stack);
+    res.status(500).json({ status: 500, data: req.body, message: err.message });
+  }
+  client.close();
+  console.log("disconnected!");
+};
+
 // getSeats();
 
-module.exports = { getSeats };
+module.exports = { getSeats, bookSeat };
